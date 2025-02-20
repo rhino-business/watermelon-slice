5bc1d079cc277ae2b5ce1805deac8bd3a3b3bc20
rhino <rhino@mailfence.com>
2025-02-20 20:59:23 +0100

    files

---
+++ watermelon.php
@@ -0,0 +1,47 @@
+<!--
+/**
+* Fresh watermelon
+* Web-illustrations / HTML illustrations
+*
+*
+* @ Author Jane Rhino <rhino@mailfence.com>
+* @ Github https://github.com/rhino-business/watermelon-slice
+* @ Copyright 2023 rhino@mailfence.com
+* @ Link https://rhino.business
+* @ https://github.com/rhino-business/watermelon-slice/blob/main/README.md
+* @ https://github.com/rhino-business/watermelon-slice/blob/main/LICENSE
+* @ Released on: May, 10 2023
+*/
+-->
+
+<!doctype html>
+ <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Watermelon</title>
+    <meta name="description" content="Fresh watermelon">
+    <link rel="stylesheet" href = "styles/watermelon.css">  
+  </head>
+   <body>
+      <div class = "container_huge"> 
+        <div class = "w_melon">
+         <div id = "sweet_piece">  
+          <div class = "sector"></div>
+           <div class = "juicy"></div>
+            <div class = "mesh"></div>
+             <div class = "rind"></div>
+              <div class = "seeds">
+                    <section id = "pyrene1"></section>
+                    <section id = "pyrene2"></section>
+                    <section id = "pyrene3"></section>
+                    <section id = "pyrene4"></section>
+                    <section id = "pyrene5"></section>
+                    <section id = "pyrene6"></section>
+                    <section id = "pyrene7"></section>
+              </div>
+          </div>
+        </div>
+      </div> 
+   </body>
+ </html>  
